2b007637c8a06c95b6ff3dc03e4cc3fd9916e8ba
Almir Jr <almir.junior@olxbr.com>
2024-09-13 10:19:33 -0300

    adjustment in hook and execution

---
+++ src/components/timer/TotalTimer.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {StyleSheet, Text, View} from "react-native";
+import {formatTime} from "../../lib/time";
+import useTimer from "../../hooks/use-timer";
+import colors from "../../styles/colors";
+
+interface TimerProps {
+    time: number;
+    isRunning: boolean;
+    onComplete?: () => void;
+    resetTime?: boolean;
+}
+
+const TotalTimer: React.FC<TimerProps> = ({ time, isRunning, onComplete, resetTime = false }) => {
+    const timer = useTimer({ time, isRunning, onComplete, resetTime });
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.textTimer}>{formatTime(timer)}</Text>
+        </View>
+    );
+};
+
+export default TotalTimer;
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 10,
+    },
+    textTimer: {
+        textAlign: 'center',
+        fontSize: 20,
+        marginBottom: 10,
+        textTransform: 'lowercase',
+        fontWeight: '900',
+        color: colors.primary,
+    },
+});
